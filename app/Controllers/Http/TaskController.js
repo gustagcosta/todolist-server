39edcd51c2d8ba6990c52ed9fcb6b8a1411c6842
gustavogcosta <gustavoprof3@gmail.com>
2020-01-10 09:13:14 -0300

    Initial commit

---
+++ app/Controllers/Http/TaskController.js
@@ -0,0 +1,100 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Task = use('App/Models/Task')
+
+/**
+ * Resourceful controller for interacting with tasks
+ */
+class TaskController {
+  /**
+   * Show a list of all tasks.
+   * GET tasks
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view, auth }) {
+    const { id } = auth.user
+    const tasks = await Task.query().where('user_id', '=', id).fetch()
+
+    return tasks
+  }
+
+  /**
+   * Create/save a new task.
+   * POST tasks
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response, auth }) {
+    const { id } = auth.user
+    const data = request.only(['content'])
+
+    const task = await Task.create({ ...data, user_id: id })
+
+    return task
+  }
+
+  /**
+   * Display a single task.
+   * GET tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+    const task = await Task.findOrFail(params.id)
+
+    return task
+  }
+
+  /**
+   * Update task details.
+   * PUT or PATCH tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+    const task = await Task.findOrFail(params.id)
+
+    const data = request.only(['content'])
+
+    task.merge(data)
+
+    await task.save()
+
+    return task
+  }
+
+  /**
+   * Delete a task with id.
+   * DELETE tasks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response, auth }) {
+    const task = await Task.findOrFail(params.id)
+
+    if (task.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
+    }
+
+    await task.delete()
+  }
+}
+
+module.exports = TaskController
---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+    async index(){
+        const users = User.all()
+        
+        return users
+    }
+    async store ({ request }) {
+        const data = request.only(['username', 'email', 'password'])
+
+        const user = await User.create(data)
+
+        return user
+    }
+}
+
+module.exports = UserController
